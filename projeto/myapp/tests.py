546165a8a5d7b3f09115eed3e653d0263da1e626
alexandre dias <xanderio@outlook.com>
2024-06-30 23:03:04 -0300

    configuraçao-de-conta
    testando

--- projeto/myapp/tests.py
+++ projeto/myapp/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.db import models
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+    def __str__(self):
+        return self.nome
+    
--- projeto/myapp/urls.py
+++ projeto/myapp/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-
 from . import views
-
-
 urlpatterns = [
-
-    path('', views.home, name='home'),  # Define a URL para a view home​
-
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
 ]
